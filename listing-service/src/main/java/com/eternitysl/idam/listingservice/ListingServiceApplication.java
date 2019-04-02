68504e78ac7e32cb178608eeb9c54c72d3b98634
DeshanKTD <deshankalupahana@gmail.com>
2019-04-02 21:55:47 +0400

    new service created

---
+++ listing-service/src/main/java/com/eternitysl/idam/listingservice/ListingServiceApplication.java
@@ -0,0 +1,25 @@
+package com.eternitysl.idam.listingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
+@Controller
+public class ListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListingServiceApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String welcomeText() {
+		return "Hello";
+	}
+
+}
